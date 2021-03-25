bed3f64b4b98d35c763ea6334f39594b7d176881
allansifuna <allansifuna324@gmail.com>
2021-03-25 18:34:16 +0300

    readme update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='Flask-RestCountries',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/allansifuna/flask-restcountries',
     license='MIT',
     author='Allan Namasaka Sifuna',
